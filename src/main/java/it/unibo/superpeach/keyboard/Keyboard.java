c5a2bfc8c522c569674b39bbdfef67387713248e
patrick sbrighi <patrick.sbrighi@studio.unibo.it>
2024-02-10 17:08:01 +0100

    Created Keyboard class and defined the keys to press

---
+++ src/main/java/it/unibo/superpeach/keyboard/Keyboard.java
@@ -0,0 +1,25 @@
+package it.unibo.superpeach.keyboard;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Keyboard extends KeyAdapter {
+    @Override
+    public void keyPressed(KeyEvent e){
+        int pressed = e.getKeyCode();
+
+        if(pressed == KeyEvent.VK_SPACE){
+            
+        }
+        else if(pressed == KeyEvent.VK_A){
+            
+        }
+        else if(pressed == KeyEvent.VK_D){
+
+        }
+        else if(pressed == KeyEvent.VK_ESCAPE){
+
+        }
+    }
+}
+  
