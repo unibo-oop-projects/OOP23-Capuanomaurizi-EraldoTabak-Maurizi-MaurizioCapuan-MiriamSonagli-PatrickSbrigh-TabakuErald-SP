abfcec49b3f9160063c9851159d422ff910a3a48
patrick sbrighi <patrick.sbrighi@studio.unibo.it>
2024-02-11 15:03:14 +0100

    Implementing keyboard

--- src/main/java/it/unibo/superpeach/keyboard/Keyboard.java
+++ src/main/java/it/unibo/superpeach/keyboard/Keyboard.java
@@ -3,23 +3,29 @@ package it.unibo.superpeach.keyboard;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class Keyboard extends KeyAdapter {
-    @Override
-    public void keyPressed(KeyEvent e){
-        int pressed = e.getKeyCode();
+import it.unibo.superpeach.player.PlayerHandler;
 
+public class Keyboard extends KeyAdapter {
+    private PlayerHandler playHand;
+
+    public Keyboard(PlayerHandler handler){
+        this.playHand = handler;
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int pressed = e.getKeyCode();
+    
         if(pressed == KeyEvent.VK_SPACE){
-            
+            playHand.getPlayer().jump();
         }
         else if(pressed == KeyEvent.VK_A){
-            
+            playHand.getPlayer().moveLeft();
         }
         else if(pressed == KeyEvent.VK_D){
-
+            playHand.getPlayer().moveRight();
         }
         else if(pressed == KeyEvent.VK_ESCAPE){
 
         }
     }
 }
-  
