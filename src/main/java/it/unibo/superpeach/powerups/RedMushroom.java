beed202f43d89ef7e140f0a677802584e0679af2
miriam sonaglia <miriam.sonaglia@studio.unibo.it>
2024-02-13 20:53:13 +0100

    Creation of the file RedMushroom

---
+++ src/main/java/it/unibo/superpeach/powerups/RedMushroom.java
@@ -0,0 +1,15 @@
+package it.unibo.superpeach.powerups;
+import javax.swing.*;
+import java.awt.*;
+
+public class RedMushroom extends PowerUp {
+    
+    public RedMushroom (double x, double y) {
+        super(x, y);
+    }
+
+    // Metodo per gestire il movimento del fungo rosso
+    public void move(){
+        
+    }
+}
