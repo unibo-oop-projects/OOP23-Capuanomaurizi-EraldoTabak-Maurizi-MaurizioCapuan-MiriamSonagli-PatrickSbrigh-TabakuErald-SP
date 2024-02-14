525508688e99f808e6c3d8335045666314f297e1
miriam sonaglia <miriam.sonaglia@studio.unibo.it>
2024-02-14 23:53:10 +0100

    added methods related to power ups

--- src/main/java/it/unibo/superpeach/game/Game.java
+++ src/main/java/it/unibo/superpeach/game/Game.java
@@ -7,58 +7,80 @@ import java.awt.image.BufferStrategy;
 
 import it.unibo.superpeach.blocks.BlocksHandler;
 import it.unibo.superpeach.blocks.graphics.Texturer;
+import it.unibo.superpeach.enemies.EnemiesHandler;
+import it.unibo.superpeach.enemies.graphics.TexturerEnemies;
 import it.unibo.superpeach.graphics.PeachMenu;
+import it.unibo.superpeach.keyboard.Keyboard;
 import it.unibo.superpeach.level.Camera;
 import it.unibo.superpeach.level.LevelHandler;
+import it.unibo.superpeach.player.Peach;
+import it.unibo.superpeach.player.PlayerHandler;
+import it.unibo.superpeach.player.graphics.PlayerTexture;
+import it.unibo.superpeach.powerups.graphics.Textures;
 
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable {
 
-    //GAME CONSTANTS
+    // GAME CONSTANTS
     private static final int MILLS_PER_SECOND = 1000;
     private static final int NANOS_PER_SECOND = 1000000000;
     private static final double TICKS_PER_SECOND = 60.0;
     private static final String GAME_NAME = "Super Peach";
     private static final int WINDOW_WIDTH = 480;
     private static final int WINDOW_HEIGHT = 360;
+    private static final int PLAYER_DEFAULT_X = 15;
+    private static final int PLAYER_DEFAULT_Y = 0;
 
-    //GAME VARIABLES
+    // GAME VARIABLES
     private boolean running;
     private static int GAME_SCALE = 2;
 
-    //GAME COMPONENTS
+    // GAME COMPONENTS
     private Thread mainGameLoop;
     private BlocksHandler blocksHandler;
     private static Texturer blocksTexturer;
     private LevelHandler levelHandler;
     private Camera camera;
     private static PeachMenu window;
+    private static PlayerTexture playerTexture;
+    private PlayerHandler playerHandler;
+    private EnemiesHandler enemiesHandler;
+    private static TexturerEnemies enemiesTexture;
+    private static Textures powerupsTexture;
 
     public static void main(String[] args) {
         window = new PeachMenu(GAME_NAME, WINDOW_WIDTH, WINDOW_HEIGHT, GAME_SCALE, new Game());
     }
 
-    public void init(){
+    public void init() {
+        enemiesHandler = new EnemiesHandler();
+        enemiesTexture = new TexturerEnemies();
+        powerupsTexture = new Textures();
         blocksTexturer = new Texturer();
         blocksHandler = new BlocksHandler();
-        levelHandler = new LevelHandler(blocksHandler, GAME_SCALE);
+        levelHandler = new LevelHandler(blocksHandler, GAME_SCALE, enemiesHandler);
         levelHandler.drawLevel();
-        camera = new Camera(WINDOW_WIDTH*GAME_SCALE, WINDOW_HEIGHT*GAME_SCALE);
+        camera = new Camera(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_SCALE);
+        playerHandler = new PlayerHandler();
+        playerTexture = new PlayerTexture();
+        playerHandler.setPlayer(new Peach(PLAYER_DEFAULT_X, PLAYER_DEFAULT_Y, 16, 32, GAME_SCALE, blocksHandler));// TOFIX
+        this.addKeyListener(new Keyboard(playerHandler));
         start();
+
     }
 
-    private synchronized void start(){
+    private synchronized void start() {
         mainGameLoop = new Thread(this);
         mainGameLoop.start();
         running = true;
     }
 
-    public synchronized void changeScale(int newScale){
+    public synchronized void changeScale(int newScale) {
         GAME_SCALE = newScale;
         window.closeWindow();
         window = new PeachMenu(GAME_NAME, WINDOW_WIDTH, WINDOW_HEIGHT, GAME_SCALE, new Game());
     }
 
-    private synchronized void stop(){
+    private synchronized void stop() {
         try {
             mainGameLoop.join();
             running = false;
@@ -74,55 +96,49 @@ public class Game extends Canvas implements Runnable{
         double ns = NANOS_PER_SECOND / ticksAmount;
         double delta = 0;
         long timer = System.currentTimeMillis();
-        int frames = 0;
-        int updates = 0;
 
-        //GAMELOOP
+        // GAMELOOP
         while (running) {
             long now = System.nanoTime();
             delta += (now - lastTime) / ns;
             lastTime = now;
             while (delta >= 1) {
                 tick();
-                updates++;
                 delta--;
             }
             if (running) {
                 render();
-                frames++;
             }
-            if(System.currentTimeMillis() - timer >= MILLS_PER_SECOND){
+            if (System.currentTimeMillis() - timer >= MILLS_PER_SECOND) {
                 timer += MILLS_PER_SECOND;
-                System.out.println("FPS: "+frames+" TPS: "+updates);
-                frames=0;
-                updates=0;
             }
         }
 
         stop();
     }
 
-    private void tick(){
-        blocksHandler.tickBlocks();
-        //player should tick camera passing his reference
-        camera.tick();
+    private void tick() {
+        playerHandler.tick();
+        enemiesHandler.tickEnemies();
+        camera.tick(playerHandler.getPlayer());
     }
 
-    private void render(){
+    private void render() {
         BufferStrategy buffStrat = this.getBufferStrategy();
-        if(buffStrat == null){
+        if (buffStrat == null) {
             this.createBufferStrategy(3);
             return;
-        } 
+        }
         Graphics g = buffStrat.getDrawGraphics();
 
         g.setColor(Color.PINK);
-        g.fillRect(0, 0, WINDOW_WIDTH*GAME_SCALE, WINDOW_HEIGHT*GAME_SCALE);
+        g.fillRect(0, 0, WINDOW_WIDTH * GAME_SCALE, WINDOW_HEIGHT * GAME_SCALE);
         g.translate(camera.getCameraX(), camera.getCameraY());
 
         blocksHandler.renderBlocks(g);
+        enemiesHandler.renderEnemies(g);
+        playerHandler.render(g);
 
-        //clean for next frame
         g.dispose();
         buffStrat.show();
     }
@@ -131,8 +147,16 @@ public class Game extends Canvas implements Runnable{
         return blocksTexturer;
     }
 
-    public void setGameScale(int s){
-        GAME_SCALE = s;
+    public static PlayerTexture getPlayerTexturer() {
+        return playerTexture;
+    }
+
+    public static TexturerEnemies getEnemyTexturer() {
+        return enemiesTexture;
+    }
+
+    public static Textures getPowerupsTextures() {
+        return powerupsTexture;
     }
 
 }
