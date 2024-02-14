ffff50941be31535754c3e59174285dfb72420c6
miriam sonaglia <miriam.sonaglia@studio.unibo.it>
2024-02-14 17:43:12 +0100

    some lines of code removed

--- src/main/java/it/unibo/superpeach/graphics/CustomButton.java
+++ src/main/java/it/unibo/superpeach/graphics/CustomButton.java
@@ -2,7 +2,6 @@ package it.unibo.superpeach.graphics;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
 
 public class CustomButton extends JButton{
     public CustomButton(String text, Color customColor, int scale) {
@@ -12,9 +11,5 @@ public class CustomButton extends JButton{
         setBackground(customColor);
         setFocusable(false);
         setBorderPainted(false);
-    }
-
-    public void addActionListener(ActionListener listener) {
-        super.addActionListener(listener);
     }
 }
