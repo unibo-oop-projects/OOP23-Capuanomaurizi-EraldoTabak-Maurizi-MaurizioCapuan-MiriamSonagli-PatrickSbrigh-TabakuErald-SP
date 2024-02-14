cdf39f876d3c56a8d0ce58d394944c35c9662d24
miriam sonaglia <miriam.sonaglia@studio.unibo.it>
2024-02-14 17:32:31 +0100

    Creation of the Volume Options

--- src/main/java/it/unibo/superpeach/graphics/PeachMenu.java
+++ src/main/java/it/unibo/superpeach/graphics/PeachMenu.java
@@ -2,6 +2,10 @@ package it.unibo.superpeach.graphics;
 import it.unibo.superpeach.game.Game;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.plaf.basic.BasicSliderUI;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -49,14 +53,61 @@ public class PeachMenu extends JFrame {
                 game.init();
             }
         });
+        panel.add(startButton);
 
-        CustomButton optionsButton = new CustomButton("OPTIONS", customColor, scale);
+        CustomButton optionsButton = new CustomButton("VOLUME", customColor, scale);
         optionsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            // Aprire un menu delle opzioni
-            JOptionPane.showMessageDialog(panel, "Opzioni del gioco");
-        }
+                // Nuovo frame per il pannello delle opzioni
+                JFrame optionsFrame = new JFrame("Volume Options");
+                optionsFrame.setSize(300, 200); // Imposta le dimensioni del frame
+                optionsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Chiudo solo il frame delle opzioni quando viene chiuso
+
+                // Nuovo pannello per le opzioni
+                JPanel optionsPanel = new JPanel();
+                optionsPanel.setLayout(new FlowLayout());
+
+                // Slider per regolare il volume
+                JSlider volumeSlider = new JSlider(JSlider.HORIZONTAL, 0, 100 , 50) {
+                    @Override
+                    public void updateUI() {
+                        setUI(new BasicSliderUI(this) {
+                            @Override
+                            public void paintThumb(Graphics g) {
+                                Rectangle knobBounds = thumbRect;
+                                int w = knobBounds.width;
+                                int h = knobBounds.height;
+                                Graphics2D g2d = (Graphics2D) g.create();
+                                g2d.setColor(customColor);
+                                g2d.fillRect(knobBounds.x, knobBounds.y, w, h);
+                                g2d.dispose();
+                            }
+                        });
+                    }
+                };
+                volumeSlider.setMajorTickSpacing(10);
+                volumeSlider.setMinorTickSpacing(1);
+                volumeSlider.setPaintTicks(true);
+                volumeSlider.setPaintLabels(true);
+
+                // Cambiamento del volume
+                volumeSlider.addChangeListener(new ChangeListener() {
+                    @Override
+                    public void stateChanged(ChangeEvent e) {
+                        JSlider source = (JSlider) e.getSource();
+                        int volume = source.getValue();
+                        System.out.println("Volume: " + volume);
+                        // Qui puoi applicare il volume alla tua musica
+                    }
+                });
+
+                optionsPanel.add(volumeSlider);
+                optionsFrame.add(optionsPanel);
+                optionsFrame.setLocationRelativeTo(null); // Centra il frame delle opzioni nello schermo
+                optionsFrame.setVisible(true);
+
+            }
         });
         panel.add(optionsButton);
 
