26c095c06d7ac370994034e23bc01bb4ef4c975a
TabakuEraldo <eraldo.tabaku@studio.unibo.it>
2024-02-16 21:38:42 +0100

    finished EnemyHandlerTest class

--- src/test/java/it/unibo/superpeach/EnemiesHandlerTest.java
+++ src/test/java/it/unibo/superpeach/EnemiesHandlerTest.java
@@ -5,8 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import it.unibo.superpeach.blocks.BlocksHandler;
 import it.unibo.superpeach.enemies.EnemiesHandler;
-import it.unibo.superpeach.enemies.Enemy;
+import it.unibo.superpeach.enemies.FlyingKoopa;
 import it.unibo.superpeach.enemies.Goomba;
+import it.unibo.superpeach.enemies.KoopaTroopa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -22,8 +23,7 @@ public class EnemiesHandlerTest {
     private static final int WIDTH = 16;
     private static final int KOOPA_HEIGHT = 24;
     private static final int GOOMBA_HEIGHT = 16;
-    private static final int SCALE1 = 1;
-    private static final int SCALE2 = 2;
+    private static final int SCALE = 1;
 
     private final EnemiesHandler enemiesHandler = new EnemiesHandler();
     private final BlocksHandler blocksHandler = new BlocksHandler();
@@ -35,7 +35,7 @@ public class EnemiesHandlerTest {
         assertNull(enemiesHandler);
 
         // Adding a new goomba to the Enemies Handler
-        enemiesHandler.addEnemy(new Goomba(X, Y, WIDTH, GOOMBA_HEIGHT, SCALE1, blocksHandler));
+        enemiesHandler.addEnemy(new Goomba(X, Y, WIDTH, GOOMBA_HEIGHT, SCALE, blocksHandler));
         assertNotNull(blocksHandler);
 
         // Checking if the enemy got added correctly by checking if the first enemy in
@@ -48,7 +48,7 @@ public class EnemiesHandlerTest {
         assertNotEquals(KOOPA_HEIGHT, enemiesHandler.getEnemies().get(0).getWidth());
         assertEquals(GOOMBA_HEIGHT, enemiesHandler.getEnemies().get(0).getHeight());
         assertNotEquals(KOOPA_HEIGHT, enemiesHandler.getEnemies().get(0).getHeight());
-        assertEquals(SCALE1, enemiesHandler.getEnemies().get(0).getScale());
+        assertEquals(SCALE, enemiesHandler.getEnemies().get(0).getScale());
         assertNotEquals(KOOPA_HEIGHT, enemiesHandler.getEnemies().get(0).getScale());
         assertEquals(blocksHandler, enemiesHandler.getEnemies().get(0).getBlocksHandler());
 
@@ -63,6 +63,19 @@ public class EnemiesHandlerTest {
 
         // Checking if the enemy is actually removed
         assertNull(enemiesHandler);
+
+        // Testing with more enemies
+        enemiesHandler.addEnemy(new KoopaTroopa(X, Y, WIDTH, KOOPA_HEIGHT, SCALE, blocksHandler));
+        enemiesHandler.addEnemy(new FlyingKoopa(X, Y, WIDTH, KOOPA_HEIGHT, SCALE, blocksHandler));
+        enemiesHandler.addEnemy(new Goomba(X, Y, WIDTH, GOOMBA_HEIGHT, SCALE, blocksHandler));
+        enemiesHandler.addEnemy(new FlyingKoopa(X, Y, WIDTH, KOOPA_HEIGHT, SCALE, blocksHandler));
+        enemiesHandler.addEnemy(new Goomba(X, Y, WIDTH, GOOMBA_HEIGHT, SCALE, blocksHandler));
+
+        // Checking if the number of enemies in List<Enemy> of the EnemyHandler is
+        // correct
+        assertEquals(5, enemiesHandler.getEnemies().size());
+        enemiesHandler.removeEnemy(enemiesHandler.getEnemies().get(1));
+        assertEquals(4, enemiesHandler.getEnemies().size());
 
     }
 }
