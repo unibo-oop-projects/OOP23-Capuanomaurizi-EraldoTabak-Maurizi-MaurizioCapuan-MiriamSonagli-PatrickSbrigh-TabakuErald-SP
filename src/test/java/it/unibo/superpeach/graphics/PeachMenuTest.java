45668d2cb7697fc698e0859632c9e586264361c0
miriam sonaglia <miriam.sonaglia@studio.unibo.it>
2024-02-17 16:30:39 +0100

    implemented Peach menu test

---
+++ src/test/java/it/unibo/superpeach/graphics/PeachMenuTest.java
@@ -0,0 +1,28 @@
+package it.unibo.superpeach.graphics;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import it.unibo.superpeach.game.Game;
+
+public class PeachMenuTest {
+
+    private static final String TITLE = "TITLE";
+    private static final int WIDTH = 16;
+    private static final int HEIGHT = 16;
+    private static final int SCALE = 1;
+    Game game = new Game();
+    
+    @Test
+    void menuTest() {
+        
+        // CREO IL MIO MENU'
+        PeachMenu menu = new PeachMenu(TITLE, WIDTH, HEIGHT, SCALE, game);
+
+        // VERIFICO SE IL MENU E' STATO CREATO CORRETTAMENTE
+        assertNotNull(menu);
+
+        // VERIFICO
+
+    }
+     
+}
