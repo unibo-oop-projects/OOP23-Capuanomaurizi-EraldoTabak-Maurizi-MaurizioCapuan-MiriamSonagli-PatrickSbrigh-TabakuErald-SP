e6b406324a8c0a3d81d43d61219d9e68c42cfd8d
patrick sbrighi <patrick.sbrighi@studio.unibo.it>
2024-02-17 19:13:18 +0100

    Added javadoc to PlayerHandler Class

--- src/main/java/it/unibo/superpeach/gameObjects/player/PlayerHandler.java
+++ src/main/java/it/unibo/superpeach/gameObjects/player/PlayerHandler.java
@@ -2,9 +2,18 @@ package it.unibo.superpeach.gameObjects.player;
 
 import java.awt.Graphics;
 
+/**
+ * This class manages the player.
+ * @author Patrick Sbrighi
+ */
 public class PlayerHandler {
     private Player player;
 
+    /**
+     * This method allows to set the player.
+     * @param p The player to set
+     * @return Return 0 if the operation was successful -1 otherwise
+     */
     public int setPlayer(final Player p) {
         if (this.player == null) {
             this.player = p;
@@ -13,10 +22,18 @@ public class PlayerHandler {
         return -1;
     }
 
+    /**
+     * This method return the player.
+     * @return Return the player
+     */
     public Player getPlayer() {
         return this.player;
     }
 
+    /**
+     * This method remove the player.
+     * @return Return 0 if the operation was successful -1 otherwise
+     */
     public int removePlayer() {
         if (this.player != null) {
             this.player = null;
@@ -25,10 +42,17 @@ public class PlayerHandler {
         return -1;
     }
 
+    /**
+     * This method calls the player tick.
+     */
     public void tick() {
         this.player.tick();
     }
 
+    /**
+     * This method calls the player render.
+     * @param g The graphics
+     */
     public void render(final Graphics g) {
         this.player.render(g);
     }
