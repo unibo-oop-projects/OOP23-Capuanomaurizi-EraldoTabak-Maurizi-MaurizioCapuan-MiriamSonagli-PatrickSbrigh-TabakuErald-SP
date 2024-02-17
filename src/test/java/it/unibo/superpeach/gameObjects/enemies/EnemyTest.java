460260b6cc73e50008e5db51eabd8caae9e0b541
TabakuEraldo <eraldo.tabaku@studio.unibo.it>
2024-02-17 15:06:06 +0100

    fixing EnemyTest class

--- src/test/java/it/unibo/superpeach/gameObjects/enemies/EnemyTest.java
+++ src/test/java/it/unibo/superpeach/gameObjects/enemies/EnemyTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import it.unibo.superpeach.game.Game;
 import it.unibo.superpeach.gameObjects.blocks.BlocksHandler;
+import it.unibo.superpeach.gameObjects.blocks.MapFixedBlock;
+import it.unibo.superpeach.gameObjects.blocks.Block.BlockType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -15,11 +17,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class EnemyTest {
 
     private static final int X1 = 12;
-    private static final int Y1 = 12;
+    private static final int Y1 = 13;
     private static final int X2 = 22;
-    private static final int Y2 = 22;
+    private static final int Y2 = 23;
     private static final int X3 = 32;
-    private static final int Y3 = 32;
+    private static final int Y3 = 33;
     private static final int WIDTH = 16;
     private static final int KOOPA_HEIGHT = 24;
     private static final int GOOMBA_HEIGHT = 16;
@@ -27,12 +29,11 @@ public class EnemyTest {
     private static final int SCALE2 = 2;
 
     private static final BlocksHandler BLOCKS_HANDLER = new BlocksHandler();
-    private Game game = new Game();
 
     @Test
     void testEnemy() {
 
-        game.init();
+        new Game().init();
 
         // Creating an enemy for each type
         Enemy koopa = new KoopaTroopa(X1, Y1, WIDTH, KOOPA_HEIGHT, SCALE1, BLOCKS_HANDLER);
@@ -71,9 +72,45 @@ public class EnemyTest {
     }
 
     @Test
-    void testQualcosa() {
-        game.init();
-        Enemy koopa = new KoopaTroopa(X1, Y1, WIDTH, KOOPA_HEIGHT, SCALE1, BLOCKS_HANDLER);
+    void testEnemyDeathCollision() {
+
+        new Game().init();
+
+        // Creating an enemy and a BlocksHandler containing a DEATH_BLOCK with the same
+        // coords of the enemy
+        // to check if enemies correctly die after collision with it
+        BlocksHandler bh = new BlocksHandler();
+        bh.addFixedBlock(new MapFixedBlock(X1, Y1, WIDTH, GOOMBA_HEIGHT, SCALE1, BlockType.DEATH_BLOCK));
+
+        Enemy koopa = new KoopaTroopa(X1, Y1 - 1, WIDTH, KOOPA_HEIGHT, SCALE1, bh);
+
+        assertTrue(koopa.getIsAlive());
+
+        // Checking if the enemy is alive after the collision
+        koopa.collision();
+        assertFalse(koopa.getIsAlive());
+
+    }
+
+    @Test
+    public void testCollisions() {
+
+        new Game().init();
+
+        BlocksHandler bh1 = new BlocksHandler();
+        bh1.addFixedBlock(new MapFixedBlock(X2, Y2, WIDTH, GOOMBA_HEIGHT, SCALE1, BlockType.BRICK));
+
+        Enemy goomba = new Goomba(X2, Y2, WIDTH, GOOMBA_HEIGHT, SCALE1, BLOCKS_HANDLER);
+
+        assertEquals(X2, goomba.getX());
+        assertEquals(Y2, goomba.getY());
+        assertFalse(goomba.getDirection());
+
+        goomba.collision();
+
+        assertTrue(goomba.getDirection());
+        assertEquals(X2, goomba.getX());
+        assertEquals(Y2, goomba.getY());
 
     }
 }
