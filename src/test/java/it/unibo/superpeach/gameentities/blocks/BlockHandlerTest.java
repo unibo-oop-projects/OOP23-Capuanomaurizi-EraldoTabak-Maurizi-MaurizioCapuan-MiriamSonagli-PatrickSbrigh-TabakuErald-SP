f8853ba5bbedd5d109777f475633e8535302b073
TabakuEraldo <eraldo.tabaku@studio.unibo.it>
2024-02-18 03:37:08 +0100

    finished BlockHandlerTest class

--- src/test/java/it/unibo/superpeach/gameentities/blocks/BlockHandlerTest.java
+++ src/test/java/it/unibo/superpeach/gameentities/blocks/BlockHandlerTest.java
@@ -7,21 +7,54 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.awt.Rectangle;
 import it.unibo.superpeach.game.Game;
 
 public class BlockHandlerTest {
 
+    private static final int X = 1;
+    private static final int Y = 1;
+    private static final int WIDTH = 16;
+    private static final int HEIGHT = 16;
+    private static final int SCALE1 = 1;
+    private static final int SCALE2 = 2;
+
     @BeforeEach
     public void initGameTest() {
         new Game().init();
-        BlocksHandler bH = new BlocksHandler();
     }
 
     @Test
     public void addBlockTest() {
 
-        bH.addFixedBlock();
+        BlocksHandler bH = new BlocksHandler();
+
+        bH.addFixedBlock(new MapFixedBlock(X, Y, WIDTH, HEIGHT, SCALE2, BlockType.BRICK));
+        bH.addFixedBlock(new MapFixedBlock(X, Y, WIDTH, HEIGHT, SCALE1, BlockType.BRICK));
+
+        assertEquals(2, bH.getBlocks().size());
+
+        assertNotEquals(bH.getBlocks().get(0), bH.getBlocks().get(1));
+
+        assertNotNull(bH.getBlocks().get(1));
+    }
+
+    @Test
+    public void removeBlockTest() {
+
+        BlocksHandler bH1 = new BlocksHandler();
+
+        bH1.addFixedBlock(new MapFixedBlock(X, Y, WIDTH, HEIGHT, SCALE2, BlockType.BRICK));
+        bH1.addFixedBlock(new MapFixedBlock(X, Y, WIDTH, HEIGHT, SCALE1, BlockType.BRICK));
+
+        assertEquals(2, bH1.getBlocks().size());
+
+        bH1.removeFixedBlock(bH1.getBlocks().get(1));
+        assertEquals(1, bH1.getBlocks().size());
+
+        assertNotEquals(new MapFixedBlock(X, Y, WIDTH, HEIGHT, SCALE1, BlockType.BRICK), bH1.getBlocks().get(0));
+
+        bH1.removeFixedBlock(bH1.getBlocks().get(0));
+        assertEquals(0, bH1.getBlocks().size());
     }
 
 }
